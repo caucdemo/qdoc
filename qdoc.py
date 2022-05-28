26079748f94ada21b512e22d4b97a9b3e958d17d
caucdemo <104818245+caucdemo@users.noreply.github.com>
2022-05-29 00:37:59 +0800

    Add files via upload

---
+++ qdoc.py
@@ -0,0 +1,9 @@
+
+import webbrowser
+
+webbrowser.open("http://qdoc.ameco.com.cn/clientView/login")
+
+webbrowser.open("http://qdoc.ameco.com.cn/profile/upload/2022/04/26/29532f5440d3a8eadb5bac7672d0484d.pdf")
+webbrowser.open("http://qdoc.ameco.com.cn/profile/upload/2021/03/19/31de3063423c46e95c17234d642f0b7d.pdf")
+webbrowser.open("http://qdoc.ameco.com.cn/profile/upload/2021/03/19/77f3b9ab27d931e61ab69aea0f9ae8af.pdf")
+webbrowser.open("http://qdoc.ameco.com.cn/clientView/relatedToMe/myProcessDetails?processId=2732")
